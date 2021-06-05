c2b732a2eda42b3cb17e9b5670af683b90c3dd2f
AbhishekPowar <mailabhipowar@gmail.com>
2021-06-05 23:54:20 +0530

    2.2 recursion init

---
+++ foundation/RecursionandBacktracking/RecursionInArrays/P0_Display_Array.java
@@ -0,0 +1,11 @@
+package foundation.RecursionandBacktracking.RecursionInArrays;
+
+import java.util.Scanner;
+
+public class P0_Display_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+    }
+}
