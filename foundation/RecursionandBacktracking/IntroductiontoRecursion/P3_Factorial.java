10c1630c0fc3631092683177e4bf575056d1d811
AbhishekPowar <mailabhipowar@gmail.com>
2021-06-25 16:37:01 +0530

    Factorial solution using java

--- foundation/RecursionandBacktracking/IntroductiontoRecursion/P3_Factorial.java
+++ foundation/RecursionandBacktracking/IntroductiontoRecursion/P3_Factorial.java
@@ -6,7 +6,7 @@ public class P3_Factorial {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.close();
+	sc.close();
 
         int fact = factorial(n);
         System.out.println(fact);
