07339fd0e2e66ab67b6c150f42c0ad7486327c29
AbhishekPowar <mailabhipowar@gmail.com>
2021-08-29 00:15:55 +0530

    P0_Flood_Fill.py solution uses visited matrix instead of array saving space

--- foundation/RecursionandBacktracking/RecursionBacktracking/P0_Flood_Fill.py
+++ foundation/RecursionandBacktracking/RecursionBacktracking/P0_Flood_Fill.py
@@ -3,26 +3,28 @@ class Solution():
     def floodFill(self, maze, sr, sc, path):
 
         # @staticmethod
-        def floodFillImpl(maze, r, c, path, visited):
+        def floodFillImpl(maze, r, c, path):
             if r == sr-1 and c == sc - 1:
                 answers.append(path)
             else:
                 directions = ((0, -1, 't'), (-1, 0, 'l'),
                               (0, 1, 'd'), (1, 0, 'r'))
-                visited.add((r, c))
+                isVisited[r][c] = 1
                 for x, y, ch in directions:
                     nr, nc = ((r+y), (c+x))
-                    if 0 <= nr < sr and 0 <= nc < sc and maze[nr][nc] == 0:
-                        if (nr, nc) not in visited:
-                            floodFillImpl(maze, nr, nc, path +
-                                          ch, visited.copy())
+                    if 0 <= nr < sr and 0 <= nc < sc and maze[nr][nc] == 0 and isVisited[nr][nc] == 0:
+                        print('hit',nr,nc)
+                        floodFillImpl(maze, nr, nc, path +
+                                      ch, )
+                isVisited[r][c] = 1
+        isVisited = [[0 for i in range(sc)] for x in range(sr)]
         answers = []
-        floodFillImpl(maze, 0, 0, '', set())
+        floodFillImpl(maze, 0, 0, '')
         return answers
 
 
 if __name__ == "__main__":
-    # rows,columns = [int(x) for x in input().split(' ')]
+    rows,columns = [int(x) for x in input().strip().split(' ')]
     maze = [
         [0, 0, 0, 0],
         [0, 0, 1, 0],
@@ -30,8 +32,9 @@ if __name__ == "__main__":
         [0, 1, 1, 0],
         [0, 0, 0, 0]
     ]
-    # maze = [[int(x) for i in input().split(' ')] for x in range(rows)]
+    maze = [[int(i) for i in input().strip().split(' ')] for _ in range(rows)]
     sr = len(maze)
     sc = len(maze[0])
+    print(maze,sr,sc)
     output = Solution().floodFill(maze, sr, sc, '')
     [print(out) for out in output]
