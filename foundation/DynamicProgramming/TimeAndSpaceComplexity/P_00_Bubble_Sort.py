55e7d771461992b40778b716093f17eb955a875e
AbhishekPowar <mailabhipowar@gmail.com>
2021-09-01 02:43:15 +0530

    bubble sort j and j+1 way

---
+++ foundation/DynamicProgramming/TimeAndSpaceComplexity/P_00_Bubble_Sort.py
@@ -0,0 +1,17 @@
+def bubbleSort(nums):
+    numsLen = len(nums)
+
+    for i in range(numsLen):
+        for j in range(0, numsLen-1-i):
+            print(f'Comparing {nums[j+1]} and {nums[j]}')
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                print(f'Swapping {nums[j]} and {nums[j+1]}')
+
+
+if __name__ == "__main__":
+    nums = [7, -2, 4, 1, 3]
+    # count = int(input())
+    # nums = [int(input()) for _ in range(count)]
+    bubbleSort(nums)
+    print(nums)
