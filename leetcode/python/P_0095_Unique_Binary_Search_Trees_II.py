54db3595626db0e49b13ea38930353a19bf8b032
AbhishekPowar <mailabhipowar@gmail.com>
2021-09-03 11:00:33 +0530

    optimized for earlier solution, removed array. using range instead; lru_cache

--- leetcode/python/P_0095_Unique_Binary_Search_Trees_II.py
+++ leetcode/python/P_0095_Unique_Binary_Search_Trees_II.py
@@ -1,4 +1,6 @@
 from functools import lru_cache
+
+
 class TreeNode():
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -8,25 +10,26 @@ class TreeNode():
 
 class Solution:
     def generateTrees(self, n: int):
-        def arrayToTree(nums):
-            if nums:
+        # leet code n ranges between 1 ot 8 for larger n use lru_cache to memoize
+        # @lru_cache
+        def arrayToTree(low, high):
+            if low <= high:
                 output = []
-                for i in range(len(nums)):
-                    leftTreeArr = arrayToTree(nums[:i])
-                    rightTreeArr = arrayToTree(nums[i+1:])
+                for i in range(low, high+1):
+                    leftTreeArr = arrayToTree(low, i-1)
+                    rightTreeArr = arrayToTree(i+1, high)
                     for leftTree in leftTreeArr:
                         for rightTree in rightTreeArr:
                             output.append(
-                                TreeNode(nums[i], leftTree, rightTree))
+                                TreeNode(i, leftTree, rightTree))
                 return output
             else:
                 return [None]
-        nums = [i for i in range(1,n+1)]
-        output = arrayToTree(nums)
+        output = arrayToTree(1, n)
         return output
 
 
 if __name__ == "__main__":
-    n = 3
+    n = 13 
     output = Solution().generateTrees(n)
-    print(output)
+    print(len(output))
