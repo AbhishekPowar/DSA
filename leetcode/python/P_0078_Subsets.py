09fbf790f49e6d20c6ac9517c077c659433d9fd5
AbhishekPowar <mailabhipowar@gmail.com>
2021-09-11 00:12:50 +0530

    bfs and backtracking solution added

--- leetcode/python/P_0078_Subsets.py
+++ leetcode/python/P_0078_Subsets.py
@@ -3,19 +3,18 @@ from typing import List
 
 class Solution:
     def subsetsRecursive(self, nums: List[int]) -> List[List[int]]:
+        def calcSubset(nums,  idx, lenOfNums, temp):
+            if idx < lenOfNums:
+                calcSubset(nums, idx+1, lenOfNums, temp[:])
+                temp.append(nums[idx])
+                calcSubset(nums, idx+1, lenOfNums, temp[:])
+            else:
+                self.allSubsets.append(temp)
         self.allSubsets = []
-        self.calcSubset(nums, 0, len(nums), [])
+        calcSubset(nums, 0, len(nums), [])
         return self.allSubsets
 
-    def calcSubset(self, nums,  idx, lenOfNums, temp):
-        if idx < lenOfNums:
-            self.calcSubset(nums, idx+1, lenOfNums, temp[:])
-            temp.append(nums[idx])
-            self.calcSubset(nums, idx+1, lenOfNums, temp[:])
-        else:
-            self.allSubsets.append(temp)
-
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsetsBitMap(self, nums: List[int]) -> List[List[int]]:
         allSubsets = []
         N = len(nums)
         for n in range(2**N):
@@ -27,8 +26,30 @@ class Solution:
             allSubsets.append(subset)
         return allSubsets
 
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def backtractHelper(nums, idx, lenOfNums, temp):
+            self.allSubsets.append(temp[:])
+            if idx < lenOfNums:
+                for i in range(idx, lenOfNums):
+                    temp.append(nums[i])
+                    backtractHelper(nums, i+1, lenOfNums, temp)
+                    temp.pop()
+        self.allSubsets = []
+        backtractHelper(nums, 0, len(nums), [])
+        return self.allSubsets
+
+    def subsetsBfs(self, nums):
+        allSubsets = [[]]
+        for i in nums:
+            size = len(allSubsets)
+            for subIdx in range(size):
+                newSubset = allSubsets[subIdx]+[i]
+                allSubsets.append(newSubset)
+        return allSubsets
+
 
 if __name__ == "__main__":
-    nums = [1, 2, 3, 4]
-    output = Solution().subsets(nums)
+    nums = [1, 2, 3]
+    # output = Solution().subsets(nums)
+    output = Solution().subsetsRecursive(nums)
     print(output)
